1ff3723c0087f3213292efb2bf6a703b0f28d024
Youness Idbakkasse <itsblanklob@gmail.com>
2023-03-05 23:44:38 +0100

    Added new template called necessary that comes with Shopify and Vite CLI instead of Adastra CLI

--- packages/create-adastra/src/templates.ts
+++ packages/create-adastra/src/templates.ts
@@ -1,4 +1,8 @@
 export const TEMPLATES = [
   { value: 'basics', title: 'Some few best practices (recommended)' },
-  { value: 'minimal', title: 'An empty Shopify theme project' }
+  { value: 'minimal', title: 'An empty Shopify theme project' },
+  {
+    value: 'necessary',
+    title: 'The most minimalistic of them all'
+  }
 ]
